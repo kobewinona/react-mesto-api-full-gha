8153a9d506225946ac97eb909a13410629efc114
kobewinona <d.klimkin@icloud.com>
2023-08-11 14:33:39 +0700

    fix: change mongodb address for noe

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 app.use(cookieParser());

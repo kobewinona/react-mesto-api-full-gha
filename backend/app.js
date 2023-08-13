72d13c47beb3e0cded2e4f8146afa7111bc7e564
kobewinona <d.klimkin@icloud.com>
2023-08-13 23:36:18 +0700

    feat: add crash test route

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/', indexRouter);
 
 app.use(errorLogger);
